c0d4605f560467bfdde348d531d6cc6a12b5217d
Rich Alloway <ralloway@perforce.com>
2024-12-16 16:28:49 +0000

    Update tests/p_lynx/lynx_dump_page_test.sh to work after 241213 (due to www.centos.org redesign)

--- tests/p_lynx/lynx_dump_page_test.sh
+++ tests/p_lynx/lynx_dump_page_test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
+# Change CHECK_FOR after 241213 due to www.centos.org redesign - Rich Alloway <ralloway@perforce.com>
+
 t_Log "Running $0 - check that lynx can dump remote page."
 
 if [ "$centos_ver" -ge "8" ]; then
@@ -11,7 +13,8 @@ fi
 
 if [ "$SKIP_QA_HARNESS" = "1" ] ; then
   URL="http://www.centos.org/"
-  CHECK_FOR="timestamp"
+  #CHECK_FOR="timestamp"	# Before 241213: https://web.archive.org/web/20241212132650/https://www.centos.org/ - RDA - 241216
+  CHECK_FOR="CentOS Project"	# After 241213 : https://web.archive.org/web/20241213163833/https://www.centos.org/ - RDA - 241216
 else
   URL="http://repo.centos.qa/qa/"
   CHECK_FOR="ks_cfg"
