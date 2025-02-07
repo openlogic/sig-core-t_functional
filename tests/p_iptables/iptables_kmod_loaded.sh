30fd7baf9c6cdd87ab98003a26b64730a22e3a61
Rich Alloway <ralloway@perforce.com>
2025-02-07 20:21:12 +0000

    ip_tables kernel module not loaded by default so attempt to load it

--- tests/p_iptables/iptables_kmod_loaded.sh
+++ tests/p_iptables/iptables_kmod_loaded.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
+# 250207 - ip_tables kernel module not loaded by default so attempt to load it before verifying that it's loaded - Rich Alloway <ralloway@perforce.com>
+
 t_Log "Running $0 - check if iptables kernel modules are loaded"
 
 if [ "$centos_ver" -ge 7 ];then
@@ -8,6 +10,9 @@ if [ "$centos_ver" -ge 7 ];then
  t_CheckExitStatus 0
  exit 0
 fi
+
+# Load ip_tables module - RDA - 250207
+modprobe ip_tables
 
 lsmod | grep "ip_tables" > /dev/null 2>&1
 
