778e8331f8bd7fee5d9a2285708516466e4f93ac
Jesse Portnoy <jesse.portnoy@perforce.com>
2025-04-28 18:05:19 +0100

    This script runs with `/bin/sh` but really, it will fail in all below places with multiple POSIX compat shells and was clearly designed to run with BASH: ``` if [ $MTA_ACCEPT == 0 ] # W: In POSIX sh, == in place of = is undefined. if [[ $mail =~ $regex ]] # W: In POSIX sh, [[ ]] is undefined. if ([ $MTA_ACCEPT == 0  ] && [ $DELIVERED == 0 ]) # W: In POSIX sh, == in place of = is undefined. egrep -q "${BASH_REMATCH[1]}\:.*stat\=Sent" /var/log/maillog # W: In POSIX sh, BASH_REMATCH is undefined. echo -e # W: In POSIX sh, echo flags are undefined. ```
    Wherever possible, I replaced `==` with `=` (which will work with BASH
    and any POSIX compat shell) but since there are other BASH specific
    features that are being used, changed to `#!/bin/bash`

--- tests/p_sendmail/20_sendmail_mta.sh
+++ tests/p_sendmail/20_sendmail_mta.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - sendmail can accept and deliver local email."
@@ -12,7 +12,7 @@ DELIVERED=1
 # send mail to localhost
 mail=$(echo -e "helo localhost\nmail from: root@localhost\nrcpt to: root@localhost\ndata\nt_functional test\n.\nquit\n" | nc -w 5 127.0.0.1 25 | grep accepted)
 MTA_ACCEPT=$?
-if [ $MTA_ACCEPT == 0 ]
+if [ $MTA_ACCEPT = 0 ]
   then
   t_Log 'Mail has been queued successfully'
 fi
@@ -25,7 +25,7 @@ fi
 regex='250\ 2\.0\.0\ ([0-9A-Za-z]*)\ Message\ accepted\ for\ delivery'
 if [[ $mail =~ $regex ]]
   then
-	for i in `seq 1 60`; do
+	for i in $(seq 1 60); do
 		egrep -q "${BASH_REMATCH[1]}\:.*stat\=Sent" /var/log/maillog
 		DELIVERED=$?
 		if [ "$DELIVERED" -ne 0 ];then
@@ -36,7 +36,7 @@ if [[ $mail =~ $regex ]]
 	done
 fi
 
-if ([ $MTA_ACCEPT == 0  ] && [ $DELIVERED == 0 ])
+if [ $MTA_ACCEPT = 0  ] && [ $DELIVERED = 0 ]
   then
   ret_val=0
   t_Log 'Mail has been delivered and removed from queue.'
