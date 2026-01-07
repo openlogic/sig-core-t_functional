2ac2474840300ed2a2ac3905290533c8af9d9737
Rich Alloway <ralloway@perforce.com>
2026-01-07 17:58:07 +0000

    Disable vsftpd IPv6 since not all CentOS 8 vagrant boxes support IPv6

--- tests/p_vsftpd/0-install_vsftpd.sh
+++ tests/p_vsftpd/0-install_vsftpd.sh
@@ -4,6 +4,10 @@
 t_Log "Running $0 - installing vsFTPd."
 t_InstallPackage vsftpd
 if [ "$centos_ver" -ge 8 ] ; then
+
+# Disable IPv6 since not all CentOS 8 vagrant boxes support IPv6 - RDA - 260107
+sed -i -e 's/^listen=NO/listen=YES/g' -e 's/^listen_ipv6=YES/listen_ipv6=NO/g' /etc/vsftpd/vsftpd.conf
+
 cp -fp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.tf_p_vsftpd
 sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd/vsftpd.conf
 fi
